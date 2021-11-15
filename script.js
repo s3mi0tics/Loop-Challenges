af022977c3b27965123d7117340b40c2ca11f19c
Colby Kauk <colby.kauk@gmail.com>
2021-11-15 13:30:44 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+for(var i=0; i<=20; i++){
+    if(i%2==1){
+        console.log(i)
+    }   
+}
+
+for(var i=100; i>=0; i--)
+    if(i%3==0){
+        console.log(i)
+    }
+
+var value=5.5
+    for(i=0; i<=5; i++){
+        console.log(value-=1.5)
+    }
+
+var sum=0
+for(i=0; i<=100; i++){
+    sum+=i 
+}
+console.log(sum)
+
+var product=1
+for(i=1;i<=12;i++){
+    product*=i
+}
+console.log(product)
